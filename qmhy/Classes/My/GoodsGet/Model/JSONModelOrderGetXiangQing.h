f4bad966430692105391ff51cba2ce6794be9043
mac <mac@bogon>
2015-11-26 13:22:57 +0800

    123

---
+++ qmhy/Classes/My/GoodsGet/Model/JSONModelOrderGetXiangQing.h
@@ -0,0 +1,13 @@
+//
+//  JSONModelOrderGetXiangQing.h
+//  qmhy
+//
+//  Created by mac on 15/11/26.
+//  Copyright © 2015年 wsy.Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JSONModelOrderGetXiangQing : NSObject
+
+@end
