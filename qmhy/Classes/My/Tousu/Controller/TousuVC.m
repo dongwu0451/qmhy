8abf01ff88478449fdc9f74d7ca375d7cd379724
mac <mac@bogon>
2015-11-30 09:51:29 +0800

    123

--- qmhy/Classes/My/Tousu/Controller/TousuVC.m
+++ qmhy/Classes/My/Tousu/Controller/TousuVC.m
@@ -7,14 +7,172 @@
 //
 
 #import "TousuVC.h"
+#import <AVFoundation/AVFoundation.h>
+#import "AFNetworking.h"
+#define kRecordAudioFile @"myRecord.caf"
 
-@interface TousuVC ()
+
+@interface TousuVC () <AVAudioRecorderDelegate>
+
+@property (nonatomic,strong) AVAudioRecorder *audioRecorder;//音频录音机
+@property (nonatomic,strong) NSTimer *timer;//录音声波监控（注意这里暂时不对播放进行监控）
+@property (weak, nonatomic) IBOutlet UIButton *record;
+@property (weak, nonatomic) IBOutlet UIButton *stop;
+@property (weak, nonatomic) IBOutlet UIProgressView *audioPower;//音频波动
 
 @end
 
 @implementation TousuVC
 
-- (IBAction)tousuBtnClick:(UIButton *)sender {
+#pragma mark - 控制器视图方法
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self setAudioSession];
+}
+
+#pragma mark - 私有方法
+/**
+ *  设置音频会话
+ */
+- (void)setAudioSession {
+    AVAudioSession *audioSession=[AVAudioSession sharedInstance];
+    //设置为播放和录音状态，以便可以在录制完之后播放录音
+    [audioSession setCategory:AVAudioSessionCategoryPlayAndRecord error:nil];
+    [audioSession setActive:YES error:nil];
+}
+
+/**
+ *  取得录音文件保存路径
+ *
+ *  @return 录音文件路径
+ */
+- (NSURL *)getSavePath {
+    NSString *urlStr=[NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
+    urlStr=[urlStr stringByAppendingPathComponent:kRecordAudioFile];
+    NSLog(@"file path:%@",urlStr);
+    NSURL *url=[NSURL fileURLWithPath:urlStr];
+    return url;
+}
+
+/**
+ *  取得录音文件设置
+ *
+ *  @return 录音设置
+ */
+- (NSDictionary *)getAudioSetting {
+    NSMutableDictionary *dicM=[NSMutableDictionary dictionary];
+    //设置录音格式
+    [dicM setObject:@(kAudioFormatLinearPCM) forKey:AVFormatIDKey];
+    //设置录音采样率，8000是电话采样率，对于一般录音已经够了
+    [dicM setObject:@(8000) forKey:AVSampleRateKey];
+    //设置通道,这里采用单声道
+    [dicM setObject:@(1) forKey:AVNumberOfChannelsKey];
+    //每个采样点位数,分为8、16、24、32
+    [dicM setObject:@(8) forKey:AVLinearPCMBitDepthKey];
+    //是否使用浮点数采样
+    [dicM setObject:@(YES) forKey:AVLinearPCMIsFloatKey];
+    //....其他设置等
+    return dicM;
+}
+
+/**
+ *  获得录音机对象
+ *
+ *  @return 录音机对象
+ */
+- (AVAudioRecorder *)audioRecorder {
+    if (!_audioRecorder) {
+        //创建录音文件保存路径
+        NSURL *url=[self getSavePath];
+        //创建录音格式设置
+        NSDictionary *setting=[self getAudioSetting];
+        //创建录音机
+        NSError *error=nil;
+        _audioRecorder=[[AVAudioRecorder alloc]initWithURL:url settings:setting error:&error];
+        _audioRecorder.delegate=self;
+        _audioRecorder.meteringEnabled=YES;//如果要监控声波则必须设置为YES
+        if (error) {
+            NSLog(@"创建录音机对象时发生错误，错误信息：%@",error.localizedDescription);
+            return nil;
+        }
+    }
+    return _audioRecorder;
+}
+
+/**
+ *  录音声波监控定制器
+ *
+ *  @return 定时器
+ */
+- (NSTimer *)timer {
+    if (!_timer) {
+        _timer=[NSTimer scheduledTimerWithTimeInterval:0.1f target:self selector:@selector(audioPowerChange) userInfo:nil repeats:YES];
+    }
+    return _timer;
+}
+
+/**
+ *  录音声波状态设置
+ */
+- (void)audioPowerChange {
+    [self.audioRecorder updateMeters];//更新测量值
+    float power= [self.audioRecorder averagePowerForChannel:0];//取得第一个通道的音频，注意音频强度范围时-160到0
+    CGFloat progress=(1.0/160.0)*(power+160.0);
+    [self.audioPower setProgress:progress];
+}
+
+#pragma mark - UI事件
+/**
+ *  点击录音按钮
+ *
+ *  @param sender 录音按钮
+ */
+
+- (IBAction)recordClick:(UIButton *)sender {
+    NSLog(@"开始录音并");
+    if (![self.audioRecorder isRecording]) {
+        [self.audioRecorder record];//首次使用应用时如果调用record方法会询问用户是否允许使用麦克风
+        self.timer.fireDate=[NSDate distantPast];
+    }
+
+}
+
+/**
+ *  点击停止按钮
+ *
+ *  @param sender 停止按钮
+ */
+- (IBAction)stopClick:(UIButton *)sender {
+    NSLog(@"结束录音并发送");
+    [self.audioRecorder stop];
+    self.timer.fireDate=[NSDate distantFuture];
+    self.audioPower.progress=0.0;
+    // 1.创建一个管理者
+    AFHTTPRequestOperationManager *mgr = [AFHTTPRequestOperationManager manager];
+    // 2.封装参数(这个字典只能放非文件参数)
+    NSMutableDictionary *params = [NSMutableDictionary dictionary];
+    params[@"methodName"] = @"adduseraudios";
+    params[@"proName"] = @20;
+    // 2.发送一个请求
+    NSString *url = @"http://139.129.26.164:8080/adminapp/WebAppController/uploadfile.html";
+    [mgr POST:url parameters:params constructingBodyWithBlock:^(id<AFMultipartFormData> formData) {
+        // 在发送请求之前会自动调用这个block
+        // 需要在这个block中添加文件参数到formData中
+        
+        /**
+         FileURL : 需要上传的文件的URL路径
+         name : 服务器那边接收文件用的参数名
+         fileName : （告诉服务器）所上传文件的文件名
+         mimeType : 所上传文件的文件类型
+         */
+        NSURL *url = [[NSBundle mainBundle] URLForResource:@"***" withExtension:@"txt"];
+        [formData appendPartWithFileURL:url name:@"file" fileName:@"test.txt" mimeType:@"text/plain" error:nil];
+    } success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        NSLog(@"上传成功");
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        NSLog(@"上传失败");
+    }];
+
     
 }
 
